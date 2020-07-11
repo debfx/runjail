c79dcfb8a62b62ae7a696cc34659f1f971c40b18
Felix Geyer <debfx@fobos.de>
2020-07-11 14:21:30 +0200

    Add some initial tests

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/cyphar/filepath-securejoin v0.2.2
 	github.com/godbus/dbus/v5 v5.0.3
+	github.com/otiai10/copy v1.2.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/seccomp/libseccomp-golang v0.9.1
 	github.com/spf13/pflag v1.0.5
---
+++ runjail_test.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"testing"
+
+	"github.com/otiai10/copy"
+)
+
+func runTest(testName string, args []string) (string, error) {
+	env := os.Environ()
+	env = append(env, "RUNJAIL_TEST_INNER=1")
+
+	cmdArgs := append([]string{"/proc/self/exe"}, args...)
+	cmdArgs = append(cmdArgs, "--ro", "testdata/scripts/"+testName, "--", "testdata/scripts/"+testName)
+
+	cmd := exec.Cmd{
+		Path:   "/proc/self/exe",
+		Args:   cmdArgs,
+		Stderr: os.Stderr,
+		Env:    env,
+	}
+	out, err := cmd.Output()
+	return strings.TrimRight(string(out), "\n"), err
+}
+
+func TestMain(m *testing.M) {
+	// inside a test, run the main binary (not the test)
+	if os.Getenv("RUNJAIL_TEST_INNER") == "1" {
+		main()
+		return
+	}
+
+	// start test runner
+	os.Exit(m.Run())
+}
+
+func assertNil(t *testing.T, obj interface{}) {
+	if obj != nil {
+		t.Fatal(obj)
+	}
+}
+
+func assertEqual(t *testing.T, expected string, actual string) {
+	if actual != expected {
+		t.Fatal(fmt.Printf("Expected '%s', got '%s'", expected, actual))
+	}
+}
+
+func createTempDataDir(t *testing.T) string {
+	tempDir, err := ioutil.TempDir("", t.Name())
+	if err != nil {
+		t.Errorf("TempDir create failed: %v", err)
+	}
+
+	t.Cleanup(func() {
+		if err := os.RemoveAll(tempDir); err != nil {
+			t.Errorf("TempDir cleanup failed: %v", err)
+		}
+	})
+
+	err = copy.Copy("testdata", tempDir)
+	if err != nil {
+		t.Errorf("copy test data failed: %v", err)
+	}
+
+	return tempDir
+}
+
+func TestCwd(t *testing.T) {
+	stdout, err := runTest("cwd", []string{"--cwd", "/tmp"})
+	assertNil(t, err)
+	assertEqual(t, "/tmp", stdout)
+}
+
+func TestCwdDefault(t *testing.T) {
+	cwd, err := os.Getwd()
+	assertNil(t, err)
+
+	stdout, err := runTest("cwd", []string{})
+	assertNil(t, err)
+	assertEqual(t, cwd, stdout)
+}
+
+func TestRo(t *testing.T) {
+	tempDir := createTempDataDir(t)
+
+	stdout, err := runTest("ro", []string{"--cwd", tempDir, "--ro", path.Join(tempDir, "data/ro")})
+	assertNil(t, err)
+	assertEqual(t, "rotest", stdout)
+}
+
+func TestBindRo(t *testing.T) {
+	tempDir := createTempDataDir(t)
+
+	stdout, err := runTest("bindro", []string{"--bind-ro", path.Join(tempDir, "data/ro") + ":/bindro"})
+	assertNil(t, err)
+	assertEqual(t, "rotest", stdout)
+}
+
+func TestBwrap(t *testing.T) {
+	bwrapBin, err := exec.LookPath("bwrap")
+	if err != nil {
+		t.Skip("bwrap not found")
+	}
+
+	stdout, err := runTest("bwrap", []string{"--backend", "bwrap"})
+	assertNil(t, err)
+	assertEqual(t, bwrapBin, stdout)
+}
